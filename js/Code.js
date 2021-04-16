15b4aa8530423d484bec12b1d4f997a585ac6eac
Parth-pixel <74820219+Parth-pixel@users.noreply.github.com>
2021-04-16 16:18:35 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+    text("A T A D A S B A", 700,150);
+    text("Hint : organized collection of structured information, or data, stored electronically in a computer system", 700,170);
+    text("C U T N I F O N",150,270);
+    text("Hint : We call it once made in sketch.js",150,300);
+// add code for displaying rest of the names and the hints.
+    
+
+}
